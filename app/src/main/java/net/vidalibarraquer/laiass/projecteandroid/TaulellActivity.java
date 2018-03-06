d56d24fd953031dc396a4fb0644a5e499ffa539f
Laia Sanahuja <laia.sase@gmail.com>
2018-03-06 13:15:41 +0100

    blblblb

---
+++ app/src/main/java/net/vidalibarraquer/laiass/projecteandroid/TaulellActivity.java
@@ -0,0 +1,19 @@
+package net.vidalibarraquer.laiass.projecteandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TaulellActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taulell);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
